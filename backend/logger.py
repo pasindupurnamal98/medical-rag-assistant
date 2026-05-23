78b17d347631cf95391319820145e35a051da5e4
pasindupurnamal98 <sasindunisal@gmail.com>
2026-05-23 14:16:05 +0530

    test logger.py

--- backend/logger.py
+++ backend/logger.py
@@ -11,4 +11,16 @@ def setup_logger(name="MedicalAssistant"):
     # Create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(ch)
+
+    return logger
+
+logger = setup_logger()
+
+logger.info("RAG Process started.")
+logger.debug("Debugging")
+logger.error("Failed to load")
+logger.critical("Critical error occurred!")
                  
