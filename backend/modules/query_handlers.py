9ab824e831ac67b8f844a6c6092b8be94d6bcb89
pasindupurnamal98 <sasindunisal@gmail.com>
2026-05-25 21:33:15 +0530

    create query_handlers.py

---
+++ backend/modules/query_handlers.py
@@ -0,0 +1,15 @@
+from logger import Logger
+
+def query_chain(chain,user_input:str):
+    try:
+        logger.debug(f"Running Chain for input: {user_input}")
+        result = chain({"query": user_input})
+        response={
+            "response": result['result'],
+            "sources": [doc.metadata.get("sources","") for doc in result["source_documents"]]
+        }
+        logger.debug(f"Chain result: {response}")
+        return response
+    except Exception as e:
+        logger.exception(f"Error in query_chain")
+        raise 
